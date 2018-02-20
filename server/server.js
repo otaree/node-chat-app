ae62c7a83e07a9ecf69ef11d0b4158fec1b92841
otaree <deep.yen.ur.hash@gmail.com>
2018-02-20 10:14:03 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,31 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+    console.log('New user connected');    
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text:' New User joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })    
+        });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });  
     });
 
     socket.on('disconnect', () => {
